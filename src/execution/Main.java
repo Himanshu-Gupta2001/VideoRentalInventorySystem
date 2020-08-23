a058601743a3c1439f45592e8eed26564928f710
Himanshu Gupta <himanshu.gupta_cs19@gla.ac.in>
2020-08-23 15:51:36 +0530

    add main() method to Main.java

---
+++ src/execution/Main.java
@@ -0,0 +1,13 @@
+/* Created by IntelliJ IDEA.
+ *  Author: Himanshu Gupta (191500345)
+ *  Date: 8/23/2020
+ *  Time: 3:43 PM
+ */
+
+package execution;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
